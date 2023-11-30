01aa0eca01bafd10484735503cf2158a33e0c9ea
clonidine <lcly@duck.com>
2023-11-30 13:01:11 -0300

    lean4

---
+++ LearningLean4.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LearningLean4` library.
+-- Import modules here that should be built as part of the library.
+import «LearningLean4».Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «LearningLean4»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
